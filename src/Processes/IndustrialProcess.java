e910b2f4401dd998af9b30060e524d247c65fec1
ulasarslan1 <125958208+ulasarslan1@users.noreply.github.com>
2025-10-06 08:40:44 +0200

    Update IndustrialProcess.java

--- src/Processes/IndustrialProcess.java
+++ src/Processes/IndustrialProcess.java
@@ -2,17 +2,17 @@ package Processes;
 
 public class IndustrialProcess extends Process {
 
-    public IndustrialProcess(String ID, String[] operations) {
-        super(ID, operations); // ✅ correct superclass call
+    public IndustrialProcess(String ID, String[] operations,double[] operationCosts, int[] resourceAmounts) {
+        super(ID, operations, operationCosts, resourceAmounts); // superclass call
     }
 
     @Override
     public double processDuration() {
-        return operations.length * 1.5;
+        return operations.length * 2.5;
     }
 
     @Override
     public void processResources() {
-        System.out.println("Resources required: Machines, Energy, Human Operators");
+        System.out.println("Uses machines and human labor.");
     }
 }
