fc80537c1dd6d2b7466b505587d0c97199c0b372
Ashwin Balaji AR <81273776+ashwin-ar@users.noreply.github.com>
2025-10-06 20:42:51 +0530

    Updating Other resources

---
+++ src/Resources/AGV.java
@@ -0,0 +1,59 @@
+package Resources;
+
+public class AGV extends NonHumanResources{
+
+	private double batteryLoad;
+	private double consumption;
+	private long chargingTime;
+	private Position position;
+	private float maxspeed;
+	private float actspeed;
+	
+	public AGV(String iD, String locName, double batteryLoad, double consumption, 
+			long chargingTime, Position position, float maxspeed,
+			float actspeed) {
+		super(iD, locName , 0 ,0);
+		this.ID = iD;
+		this.batteryLoad = batteryLoad;
+		this.consumption = consumption;
+		this.chargingTime = chargingTime;
+		this.position = position;
+		this.maxspeed = maxspeed;
+		this.actspeed = actspeed;
+	}
+	
+	
+	public double getConsumption() {
+		return consumption;
+	}
+
+	
+	public double getBatteryLoad() {
+        return batteryLoad;
+    }
+ 
+	public void setData(String id, double battery, double cons, 
+			long time, Position pos, float maxspeed, float actspeed) {
+		this.ID = id;
+		this.batteryLoad = battery;
+		this.consumption = cons;
+		this.chargingTime = time;
+		this.position = pos;
+		this.maxspeed = maxspeed;
+		this.actspeed = actspeed;
+	}
+	
+	@Override
+	public String getData() {
+		return "AGV [ID = " + ID + ", batteryLoad = " + "%" +  batteryLoad + ", consumption = " + consumption + ", chargingTime = "
+				+ chargingTime + " m" +  ", position = " + position + ", maxspeed = " + maxspeed + " km/h"+ ", actspeed = " + actspeed + " km/h" + "]";
+	}
+
+
+	@Override
+	public String toString() {
+		return ID;
+	}
+	
+	
+}
