09adc95d3b4004730d00589270e43ea90f7b450c
Adithyaprasha <adithyaprasha@gmail.com>
2025-10-06 19:33:31 +0530

    Update IOperation.java

--- src/Processes/operation/IOperation.java
+++ src/Processes/operation/IOperation.java
@@ -2,65 +2,31 @@ package self_assignment_2;
 
 import java.util.Arrays;
 
-// ====================================
-// Abstract Class: IOperation
-// ====================================
+package Processes;
+
 public abstract class IOperation {
-	
-	private String operationID;
-	private String description;
-	private long nominalTime;
-	private Object[] resources;  // can hold either AGV or HumanResources
-	private int resourceCount = 0;
-	
-	// Constructor
-	public IOperation(String operationID, String description, long nominalTime, int maxResources) {
-		this.operationID = operationID;
-		this.description = description;
-		this.nominalTime = nominalTime;
-		this.resources = new Object[maxResources];
-	}
-	
-	// Getter for nominal time
-	public long getNominalTime() {
-		return nominalTime;
-	}
-	
-	// Add a resource (AGV or HumanResources)
-	public void addResources(Object resource) {
-		if (resourceCount < resources.length) {
-			resources[resourceCount] = resource;
-			resourceCount++;
-		} else {
-			System.out.println("No more resources can be added.");
-		}
-	}
-	
-	// Get resources
-	public Object[] getResources() {
-		return resources;
-	}
-	
-	// Set new data
-	public void setData(String id, String description, long nominalTime) {
-	     this.operationID = id;
-	     this.description = description;
-	     this.nominalTime = nominalTime;
-	}
-	
-	// Get all operation data
-	public String getData() {
-		return "IOperation [operationID = " + operationID + 
-				", description = " + description + 
-				", nominalTime = " + nominalTime + " min" + 
-				", resources = " + Arrays.toString(resources) + "]";
-	}
-	
-	@Override
-	public String toString() {
-		return operationID;
-	}
-	
-	// Abstract method — must be defined by subclasses
-	public abstract void executeOperation();
+    protected String operationID;
+    protected String operationName;
+    protected double duration;  // in hours
+    protected int resourceCount;
+
+    public IOperation(String operationID, String operationName, double duration, int resourceCount) {
+        this.operationID = operationID;
+        this.operationName = operationName;
+        this.duration = duration;
+        this.resourceCount = resourceCount;
+    }
+
+    // Abstract methods
+    public abstract void performOperation();
+    public abstract double calculateCost();
+
+    // Common method
+    public void showOperationInfo() {
+        System.out.println("Operation ID: " + operationID);
+        System.out.println("Operation Name: " + operationName);
+        System.out.println("Duration: " + duration + " hours");
+        System.out.println("Resources Used: " + resourceCount);
+    }
 }
+
